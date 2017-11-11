d028e40a1f3be768a06fd517c7de632d1155d661
HavaH123 <sri.subramanian@grabtaxi.com>
2017-11-11 15:27:16 +0800

    Add form in Wallets page

--- src/assets/styles.js
+++ src/assets/styles.js
@@ -9,5 +9,31 @@ export default StyleSheet.create({
     appName: {
         fontSize: 20,
         fontFamily: 'sans-serif-medium'
+    },
+    container: {
+        justifyContent: 'center',
+        marginTop: 50,
+        padding: 20,
+        backgroundColor: '#ffffff',
+    },
+    title: {
+        fontSize: 30,
+        alignSelf: 'center',
+        marginBottom: 30
+    },
+    buttonText: {
+        fontSize: 18,
+        color: 'white',
+        alignSelf: 'center'
+    },
+    button: {
+        height: 36,
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        borderWidth: 1,
+        borderRadius: 8,
+        marginBottom: 10,
+        alignSelf: 'stretch',
+        justifyContent: 'center'
     }
 });
--- src/views/Wallets.js
+++ src/views/Wallets.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet,TextInput, Button } from 'react-native';
+var t = require('tcomb-form-native');
 
 import Toolbar from 'components/Toolbar';
 import styles from 'assets/styles';
+
+let Form = t.form.Form;
+
+let Wallet = t.struct({
+  walletName: t.String,              // a required string
+});
 
 export default class Home extends React.Component {
 
@@ -10,12 +17,32 @@ export default class Home extends React.Component {
         drawerLabel: "Wallets"
     }
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            walletName: ''
+        };
+    }
+
+    addWallet() {
+        alert('Saved lah!!')
+    }
+
     render() {
         return(
-            <View style={styles.center}>
-                <Text>
-                    The Wallets page is WIP!
-                </Text>
+            <View style={styles.container}>
+                <Form
+                    ref="walletForm"
+                    type={Wallet}
+                    options={{}}
+                />
+
+                <Button
+                    onPress={this.addWallet}
+                    title="+ Add Wallet"
+                    style={styles.button}
+                    accessibilityLabel="Click to Add Wallet"
+                />
             </View>
         );
     }
